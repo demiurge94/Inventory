7477cda62416ebecc0c8f3db182be0ee31aec891
Erick <erickvicencio@gmail.com>
2013-09-21 22:14:42 -0700

    second commit

---
+++ Product/main.cpp
@@ -0,0 +1,15 @@
+// Product Inventory Project
+// 
+
+#include <iostream>
+#include<string>
+#include "Product.h"
+using namespace std;	
+
+
+int main(){
+	cout<<"Hello World!"<<endl;
+	
+	system("pause");
+	
+}
